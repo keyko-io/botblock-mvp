bdee27592e00d13ede58b8e606f60d718d48b8a0
Fabriziogianni7 <fabrizio@keyko.io>
2023-10-11 13:19:12 +0200

    fetch robots.txt in contractInteraction

--- packages/nextjs/components/example-ui/ContractInteraction.tsx
+++ packages/nextjs/components/example-ui/ContractInteraction.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import { CopyIcon } from "./assets/CopyIcon";
 import { DiamondIcon } from "./assets/DiamondIcon";
@@ -8,17 +9,49 @@ import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 
 export const ContractInteraction = () => {
   const [visible, setVisible] = useState(true);
-  const [newGreeting, setNewGreeting] = useState("");
+  const [url, setUrl] = useState("");
+  const [botList, setBotList] = useState<string[]>([]);
 
   const { writeAsync, isLoading } = useScaffoldContractWrite({
     contractName: "YourContract",
     functionName: "setGreeting",
-    args: [newGreeting],
+    args: [url],
     value: parseEther("0.01"),
     onBlockConfirmation: txnReceipt => {
       console.log("📦 Transaction blockHash", txnReceipt.blockHash);
     },
   });
+
+  const alertInteraction = async () => {
+    alert("scraping website and generating a new robots.txt");
+    getRobotsTxt();
+  };
+
+  const getRobotsTxt = async () => {
+    try {
+      const response = await fetch(`${url}/robots.txt`);
+      const file = await response.text();
+      console.log(file)
+      const blist = []
+      if (file.includes("User-agent: GPTBot")) {
+        blist.push("GPTBot");
+      }
+      if (file.includes("User-agent: CCBot")) {
+        blist.push("CCBot");
+      }
+      if (file.includes("User-agent: Yandex")) {
+        blist.push("Yandex");
+      }
+      if (file.includes("User-agent: PetalBot")) {
+        blist.push("PetalBot");
+      }
+
+      setBotList(blist);
+
+    } catch (error: any) {
+      console.error(`Error fetching robots.txt: ${error.message}`);
+    }
+  };
 
   return (
     <div className="flex bg-base-300 relative pb-10">
@@ -52,20 +85,20 @@ export const ContractInteraction = () => {
         </div>
 
         <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 rounded-2xl shadow-lg border-2 border-primary">
-          <span className="text-4xl sm:text-6xl text-black">Set a Greeting_</span>
+          <span className="text-4xl sm:text-6xl text-black">Input Website Url_</span>
 
           <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
             <input
               type="text"
               placeholder="Write your greeting here"
               className="input font-bai-jamjuree w-full px-5 bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white uppercase"
-              onChange={e => setNewGreeting(e.target.value)}
+              onChange={e => setUrl(e.target.value)}
             />
             <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
               <div className="flex rounded-full border-2 border-primary p-1">
                 <button
                   className="btn btn-primary rounded-full capitalize font-normal font-white w-24 flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
-                  onClick={() => writeAsync()}
+                  onClick={() => alertInteraction()}
                   disabled={isLoading}
                 >
                   {isLoading ? (
@@ -81,8 +114,13 @@ export const ContractInteraction = () => {
           </div>
 
           <div className="mt-4 flex gap-2 items-start">
-            <span className="text-sm leading-tight">Price:</span>
-            <div className="badge badge-warning">0.01 ETH + Gas</div>
+            <span className="text-sm leading-tight">Your websit is protected from:</span>
+            <div className="badge badge-warning">
+              {
+                botList.map((item,_) => <p key={_}>{item}, </p>)
+              }
+
+            </div>
           </div>
         </div>
       </div>
