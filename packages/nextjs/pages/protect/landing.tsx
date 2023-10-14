b5f3743decd6f6d52fb4977a7b1862002994978d
juam <juanmanso@gmail.com>
2023-10-14 03:26:05 +0200

    feat(web): add loading state to cta button

--- packages/nextjs/pages/protect/landing.tsx
+++ packages/nextjs/pages/protect/landing.tsx
@@ -8,20 +8,28 @@ const CTA_TEXT = "Submit";
 
 const Landing = () => {
   const [url, setUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleOnSubmit = () => {
+    setIsLoading(true);
     alert(url);
+    setIsLoading(false);
   };
+
   return (
     <div>
       <h1 className="text-4xl sm:text-6xl">{TITLE}</h1>
       <h3 className="text-xl sm:text-2xl">{DESCRIPTION}</h3>
-      <div>
-        <input type="text" placeholder={INPUT_PLACEHOLDER} onChange={e => setUrl(e.target.value)} />
-      </div>
-      {!url && (
-        <button onClick={handleOnSubmit}>
-          {CTA_TEXT} <ArrowSmallRightIcon />
+      <input type="text" placeholder={INPUT_PLACEHOLDER} onChange={e => setUrl(e.target.value)} />
+      {!!url && (
+        <button onClick={handleOnSubmit} disabled={isLoading}>
+          {isLoading ? (
+            <span className="loading loading-spinner loading-sm"></span>
+          ) : (
+            <>
+              {CTA_TEXT} <ArrowSmallRightIcon className="w-3 h-3 mt-0.5" />
+            </>
+          )}
         </button>
       )}
     </div>
