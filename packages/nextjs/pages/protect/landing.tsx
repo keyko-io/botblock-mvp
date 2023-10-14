c1da6b60afe6e581b88e8d6d2dd2ce024334e146
juam <juanmanso@gmail.com>
2023-10-14 03:23:15 +0200

    feat(web): add basic interaction between input and cta button

--- packages/nextjs/pages/protect/landing.tsx
+++ packages/nextjs/pages/protect/landing.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ArrowSmallRightIcon } from "@heroicons/react/24/outline";
 
 const TITLE = "Title";
@@ -6,16 +7,23 @@ const INPUT_PLACEHOLDER = "Insert the URL of your site here";
 const CTA_TEXT = "Submit";
 
 const Landing = () => {
+  const [url, setUrl] = useState("");
+
+  const handleOnSubmit = () => {
+    alert(url);
+  };
   return (
     <div>
       <h1 className="text-4xl sm:text-6xl">{TITLE}</h1>
       <h3 className="text-xl sm:text-2xl">{DESCRIPTION}</h3>
       <div>
-        <input type="text" placeholder={INPUT_PLACEHOLDER} />
+        <input type="text" placeholder={INPUT_PLACEHOLDER} onChange={e => setUrl(e.target.value)} />
       </div>
-      <button>
-        {CTA_TEXT} <ArrowSmallRightIcon />
-      </button>
+      {!url && (
+        <button onClick={handleOnSubmit}>
+          {CTA_TEXT} <ArrowSmallRightIcon />
+        </button>
+      )}
     </div>
   );
 };
