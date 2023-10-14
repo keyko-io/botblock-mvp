5ca2186680fb65bf1bad72aad212de8e0c6b7061
juam <juanmanso@gmail.com>
2023-10-14 17:55:53 +0200

    feat(web): add redirect option when robots is not init on robots-txt

--- packages/nextjs/pages/protect/robots-txt.tsx
+++ packages/nextjs/pages/protect/robots-txt.tsx
@@ -1,4 +1,26 @@
-export const RobotsTxt = ({}) => {
+import { useRouter } from "next/router";
+import { useRobotsContext } from "~~/context/RobotsContext";
+
+export const RobotsTxt = () => {
+  const router = useRouter();
+  const { originalRobotsTxt } = useRobotsContext();
+
+  const redirectToLanding = () => router.replace("/protect/landing");
+
+  if (!originalRobotsTxt) {
+    return (
+      <div className="p-32 flex-grow" data-theme="exampleUi">
+        <h1 className="text-4xl sm:text-6xl mb-32">Please go to the previous page to initiate the protect process</h1>
+        <button
+          className="btn btn-primary w-32 rounded-full capitalize font-normal font-white flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
+          onClick={redirectToLanding}
+        >
+          Click here
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="p-32 flex-grow" data-theme="exampleUi">
       <h1 className="text-4xl sm:text-6xl">On this page the CC user would analyze the robots.txt</h1>
