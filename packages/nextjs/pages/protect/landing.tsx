455429c9428d77651d2cd7417dff60b2a2c2ad47
juam <juanmanso@gmail.com>
2023-10-14 03:26:57 +0200

    feat(web): copy-paste some styling examples

--- packages/nextjs/pages/protect/landing.tsx
+++ packages/nextjs/pages/protect/landing.tsx
@@ -17,20 +17,37 @@ const Landing = () => {
   };
 
   return (
-    <div>
+    <div className="p-32 flex-grow" data-theme="exampleUi">
       <h1 className="text-4xl sm:text-6xl">{TITLE}</h1>
       <h3 className="text-xl sm:text-2xl">{DESCRIPTION}</h3>
-      <input type="text" placeholder={INPUT_PLACEHOLDER} onChange={e => setUrl(e.target.value)} />
+      <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
+        <input
+          type="text"
+          placeholder={INPUT_PLACEHOLDER}
+          className="input font-bai-jamjuree w-full px-5 bg-[url('/assets/gradient-bg.png')] bg-[length:100%_100%] border border-primary text-lg sm:text-2xl placeholder-white uppercase"
+          onChange={e => setUrl(e.target.value)}
+        />
+      </div>
       {!!url && (
-        <button onClick={handleOnSubmit} disabled={isLoading}>
-          {isLoading ? (
-            <span className="loading loading-spinner loading-sm"></span>
-          ) : (
-            <>
-              {CTA_TEXT} <ArrowSmallRightIcon className="w-3 h-3 mt-0.5" />
-            </>
-          )}
-        </button>
+        <div className="mt-8 flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-5">
+          <div className="flex rounded-full border border-primary p-1 flex-shrink-0">
+            <div className="flex rounded-full border-2 border-primary p-1">
+              <button
+                className="btn btn-primary w-32 rounded-full capitalize font-normal font-white flex items-center gap-1 hover:gap-2 transition-all tracking-widest"
+                onClick={handleOnSubmit}
+                disabled={isLoading}
+              >
+                {isLoading ? (
+                  <span className="loading loading-spinner loading-sm"></span>
+                ) : (
+                  <>
+                    {CTA_TEXT} <ArrowSmallRightIcon className="w-3 h-3 mt-0.5" />
+                  </>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
