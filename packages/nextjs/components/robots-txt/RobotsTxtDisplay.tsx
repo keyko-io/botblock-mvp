3ac08c62a52f6c51c9e185632a08418cdd38ab13
juam <juanmanso@gmail.com>
2023-10-16 01:33:21 +0200

    feat(web): add copy to clipboard button to robots txt display

--- packages/nextjs/components/robots-txt/RobotsTxtDisplay.tsx
+++ packages/nextjs/components/robots-txt/RobotsTxtDisplay.tsx
@@ -1,14 +1,26 @@
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import toast from "react-hot-toast";
 import { useRobotsContext } from "~~/context/RobotsContext";
 
 export const RobotsTxtDisplay = () => {
   const { rewrittenRobots } = useRobotsContext();
-  return (
+
+  return !!rewrittenRobots ? (
     <div>
       <pre className="overflow-auto bg-slate-600 p-4 h-96">
         <code className="text-zinc-200" lang="language-yaml">
           {rewrittenRobots}
         </code>
       </pre>
+      <div className="mt-16">
+        <CopyToClipboard text={rewrittenRobots} onCopy={() => toast.success("Successfully copied")}>
+          <button className="btn btn-primary w-48 rounded-full capitalize font-normal font-white flex items-center gap-1 hover:gap-2 transition-all tracking-widest">
+            Copy to clipboard
+          </button>
+        </CopyToClipboard>
+      </div>
     </div>
+  ) : (
+    <></>
   );
 };
