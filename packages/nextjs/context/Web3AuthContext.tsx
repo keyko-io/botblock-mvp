3a1413d7305788b83259cac09bfa969815d4a0d1
juam <juanmanso@gmail.com>
2023-10-17 01:41:59 +0200

    feat(context): init web3auth context

---
+++ packages/nextjs/context/Web3AuthContext.tsx
@@ -0,0 +1,47 @@
+import { PropsWithChildren, useState } from "react";
+import { createCtx } from ".";
+import { Web3Auth } from "@web3auth/modal";
+
+const CLIENT_ID = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID;
+
+// State variables only
+type Web3AuthContextState = {
+  web3Auth: Web3Auth;
+};
+
+// This interface differentiates from State
+// because it holds any other option or fx
+// that handle the state in some way
+interface Web3AuthContext extends Web3AuthContextState {
+  initWeb3Auth: () => Promise<void>;
+}
+
+const INITIAL_STATE: Web3AuthContextState = {
+  web3Auth: new Web3Auth({
+    clientId: CLIENT_ID ?? "",
+    web3AuthNetwork: "sapphire_devnet", // Web3Auth Network
+    chainConfig: {
+      chainNamespace: "eip155",
+      chainId: "0x1",
+      rpcTarget: "https://rpc.ankr.com/eth",
+      displayName: "Ethereum Devnet",
+      blockExplorer: "https://goerli.etherscan.io",
+      ticker: "ETH",
+      tickerName: "Ethereum",
+    },
+  }),
+};
+
+const [useContext, Web3AuthContextProvider] = createCtx<Web3AuthContext>("Web3AuthContext");
+
+export const RobotsProvider = ({ children }: PropsWithChildren) => {
+  const [state] = useState<Web3AuthContextState>(INITIAL_STATE);
+
+  const initWeb3Auth = async () => {
+    await state?.web3Auth.initModal();
+  };
+
+  return <Web3AuthContextProvider value={{ ...state, initWeb3Auth }}>{children}</Web3AuthContextProvider>;
+};
+
+export const useWeb3AuthContext = useContext;
