a89e4235114495d1c3773c1cd7129b4aae5a0d39
Fabriziogianni7 <fabrizio@keyko.io>
2023-10-21 12:39:16 +0200

    update recap component (to improve)

--- packages/nextjs/components/unlock/Recap.tsx
+++ packages/nextjs/components/unlock/Recap.tsx
@@ -6,11 +6,12 @@ const Recap = ({ plan }: { plan: Plan }) => {
     <div className="bg-white rounded-md shadow-md p-4 mb-4">
       <h2 className="text-2xl font-bold mb-4">Recap</h2>
       <ul className="list-disc pl-6">
-        <li className="mb-2">{`Field 1: ${plan.contentCreator}`}</li>
-        <li className="mb-2">{`Field 2: ${plan.expirationBlock}`}</li>
-        <li className="mb-2">{`Field 3: ${plan.paymentTokenAddress}`}</li>
-        <li className="mb-2">{`Field 4: ${plan.price}`}</li>
-        <li className="mb-2">{`Field 4: ${plan.uri}`}</li>
+        <li className="mb-2">{`Website ${plan.uri}`}</li>
+        <li className="mb-2">{`Content Creator Address: ${plan.contentCreator}`}</li>
+        <li className="mb-2">{`Duration: 3 Months`}</li>
+        {/* <li className="mb-2">{`Duration: ${plan.expirationBlock}`}</li> */}
+        <li className="mb-2">{`Token to receive upon subscription: ${plan.paymentTokenAddress}`}</li>
+        <li className="mb-2">{`Price: ${plan.price} USDC`}</li>
       </ul>
     </div>
   );
