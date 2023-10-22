5fb0b039ad71928011564960211b148ede4ef361
juam <juanmanso@gmail.com>
2023-10-22 12:08:35 +0200

    feat(web): map token address with name

--- packages/nextjs/pages/subscribe/index.tsx
+++ packages/nextjs/pages/subscribe/index.tsx
@@ -2,6 +2,19 @@ import { useCallback, useEffect, useState } from "react";
 import { Plan } from "~~/context/Types";
 import { useWeb3AuthContext } from "~~/context/Web3AuthContext";
 
+type TokenAddress =
+  | "0x6B175474E89094C44Da98b954EedeAC495271d0F"
+  | "0xdAC17F958D2ee523a2206206994597C13D831ec7"
+  | "0x179522635726710Dd7D2035a81d856de4Aa7836c"
+  | "0x4d224452801ACEd8B2F0aebE155379bb5D594381";
+
+const tokenAddressMap: Record<TokenAddress, string> = {
+  "0x6B175474E89094C44Da98b954EedeAC495271d0F": "DAI",
+  "0xdAC17F958D2ee523a2206206994597C13D831ec7": "USDT",
+  "0x179522635726710Dd7D2035a81d856de4Aa7836c": "USDC",
+  "0x4d224452801ACEd8B2F0aebE155379bb5D594381": "ApeCoin",
+};
+
 const Subscribe = () => {
   const { getPlans, subsContract } = useWeb3AuthContext();
   const [plans, setPlans] = useState<Plan[]>();
@@ -26,7 +39,7 @@ const Subscribe = () => {
       <h1 className="text-4xl sm:text-6xl">Subscribe</h1>
       <h3 className="text-xl sm:text-2xl">Click on any website you want to purchase on the list below</h3>
       <div className="grid grid-cols-2 gap-4">
-        <div className="container w-screen pr-16">
+        <div className="container w-fit">
           <div className="bg-white shadow-md overflow-x-auto">
             <table className="w-full border-collapse">
               <thead>
@@ -34,7 +47,8 @@ const Subscribe = () => {
                   <th className="border p-2">Website</th>
                   <th className="border p-2">Content creator address</th>
                   <th className="border p-2">Duration</th>
-                  <th className="border p-2">Payment token</th>
+                  {/* Left just for debugging */}
+                  {/* <th className="border p-2">Payment token</th> */}
                   <th className="border p-2">Price</th>
                   <th className="border p-2"></th>
                 </tr>
@@ -50,8 +64,10 @@ const Subscribe = () => {
                           <td className="border p-2 text-center">
                             {plan.expirationBlock} Month{plan.expirationBlock !== "1" && "s"}
                           </td>
-                          <td className="border p-2">{plan.paymentTokenAddress}</td>
-                          <td className="border p-2 text-end">{plan.price}</td>
+                          {/* <td className="border p-2">{plan.paymentTokenAddress}</td> */}
+                          <td className="border p-2 text-end">
+                            {plan.price} {tokenAddressMap[plan.paymentTokenAddress as TokenAddress]}
+                          </td>
                           <td className="border p-2 text-center">
                             <button
                               className="btn btn-primary w-32 rounded-full capitalize font-normal font-white flex items-center transition-all tracking-widest"
