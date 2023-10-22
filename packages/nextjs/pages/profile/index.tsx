6525c21a2ca0beee27608f7c58d1a5b5e53482e2
juam <juanmanso@gmail.com>
2023-10-22 15:22:14 +0200

    feat(web): list users plans

--- packages/nextjs/pages/profile/index.tsx
+++ packages/nextjs/pages/profile/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Web3AuthConnectButton } from "~~/components/Header/components/Web3AuthConnectButton";
-import { Order, Plan } from "~~/context/Types";
+import { Order, Plan, TokenAddress, tokenAddressMap } from "~~/context/Types";
 import { useWeb3AuthContext } from "~~/context/Web3AuthContext";
 
 const Profile = () => {
@@ -42,8 +42,41 @@ const Profile = () => {
   return (
     <div className="p-32 flex-grow" data-theme="exampleUi">
       <h1 className="text-2xl sm:text-3xl">Welcome to your profile, {username}</h1>
+      <h1>Your Orders</h1>
       <p>{JSON.stringify(userOrders)}</p>
-      <p>{JSON.stringify(userPlans)}</p>
+      <div className="grid grid-cols-2 gap-4">
+        <div className="container w-fit">
+          <div className="bg-gray-200 shadow-md overflow-x-auto rounded p-4">
+            <h2 className="text-2xl font-bold mb-4">Your plans</h2>
+            <table className="w-full border-collapse bg-white">
+              <thead>
+                <tr>
+                  <th className="border p-2">Website</th>
+                  <th className="border p-2">Duration</th>
+                  <th className="border p-2">Price</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userPlans?.length ? (
+                  userPlans.map((plan, index) => (
+                    <tr key={index}>
+                      <td className="border p-2 text-center">{plan.uri}</td>
+                      <td className="border p-2 text-center">
+                        {plan.expirationBlock} Month{plan.expirationBlock !== "1" && "s"}
+                      </td>
+                      <td className="border p-2 text-end">
+                        {plan.price} {tokenAddressMap[plan.paymentTokenAddress as TokenAddress]}
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <p> Looks like there are no plans created with this address!</p>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
