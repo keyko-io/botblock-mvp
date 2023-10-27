e275b84779f13e75606a6cbca2ad1657057e5db7
juam <juanmanso@gmail.com>
2023-10-27 10:27:00 +0200

    feat(web): tweak status page to show plan information when available

--- packages/nextjs/pages/subscribe/status/[[planId]].tsx
+++ packages/nextjs/pages/subscribe/status/[[planId]].tsx
@@ -1,13 +1,55 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { Button } from "~~/components/Button";
+import { Loader } from "~~/components/Loader";
+import PlanDetailsBox from "~~/components/PlanDetailsBox";
+import { Plan } from "~~/context/Types";
+import { useWeb3AuthContext } from "~~/context/Web3AuthContext";
 
 const StatusPage = () => {
+  const { getPlans, subsContract } = useWeb3AuthContext();
+  const [plan, setPlan] = useState<Plan>();
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
+
   const planId = router.query.planId as string;
+
+  const redirectToSubscribeLanding = () => {
+    router.push("/subscribe");
+  };
+
+  useEffect(() => {
+    const setPlanIfExists = async () => {
+      const plans = await getPlans();
+      const foundPlan = plans?.find(plan => plan.planId === planId);
+
+      if (foundPlan) {
+        setPlan(foundPlan);
+      }
+      setIsLoading(false);
+    };
+
+    if (subsContract && !plan && planId) {
+      setPlanIfExists();
+    }
+  }, [plan, planId, getPlans, subsContract]);
 
   return (
     <div className="p-32 flex-grow" data-theme="exampleUi">
-      <h1 className="text-4xl sm:text-6xl">Status page for planId: {planId}</h1>
-      <h3 className="text-xl sm:text-2xl">Details on your purchase</h3>
+      {plan ? (
+        <>
+          <h1 className="text-4xl sm:text-6xl">Status page for {plan.uri}</h1>
+          <h3 className="text-xl sm:text-2xl">Details of your purchase</h3>
+          <PlanDetailsBox plan={plan} />
+        </>
+      ) : isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="text-4xl sm:text-6xl mb-16">{`Oops! Looks like this sub' plan does not exists`}</h1>
+          <Button title={"Click here to see available plans"} onClick={redirectToSubscribeLanding} />
+        </>
+      )}
     </div>
   );
 };
