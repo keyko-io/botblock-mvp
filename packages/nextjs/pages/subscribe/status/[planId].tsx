1006c07a1405aa10a3c13628b96dad36b7eeea9e
juam <juanmanso@gmail.com>
2023-10-27 11:40:35 +0200

    feat(web): check for user's orders to validate their access to status

--- packages/nextjs/pages/subscribe/status/[planId].tsx
+++ packages/nextjs/pages/subscribe/status/[planId].tsx
@@ -3,12 +3,13 @@ import { useRouter } from "next/router";
 import { Button } from "~~/components/Button";
 import { Loader } from "~~/components/Loader";
 import PlanDetailsBox from "~~/components/PlanDetailsBox";
-import { Plan } from "~~/context/Types";
+import { Order, Plan } from "~~/context/Types";
 import { useWeb3AuthContext } from "~~/context/Web3AuthContext";
 
 const StatusPage = () => {
-  const { getPlans, subsContract } = useWeb3AuthContext();
+  const { address, getOrders, getPlans, subsContract } = useWeb3AuthContext();
   const [plan, setPlan] = useState<Plan>();
+  const [order, setOrder] = useState<Order>();
   const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
 
@@ -17,6 +18,21 @@ const StatusPage = () => {
   const redirectToSubscribeLanding = () => {
     router.push("/subscribe");
   };
+
+  useEffect(() => {
+    const checkUserOrdersForMatch = async () => {
+      const orders = await getOrders();
+      const foundOrder = orders?.find(order => order.buyer === address && order.plan.planId === planId);
+      if (foundOrder) {
+        setOrder(foundOrder);
+      }
+      setIsLoading(false);
+    };
+
+    if (subsContract && address) {
+      checkUserOrdersForMatch();
+    }
+  }, [address, getOrders, planId, subsContract]);
 
   useEffect(() => {
     const setPlanIfExists = async () => {
@@ -36,11 +52,11 @@ const StatusPage = () => {
 
   return (
     <div className="p-32 flex-grow" data-theme="exampleUi">
-      {plan ? (
+      {order ? (
         <>
-          <h1 className="text-4xl sm:text-6xl">Status page for {plan.uri}</h1>
+          <h1 className="text-4xl sm:text-6xl">Status page for {order.plan.uri}</h1>
           <h3 className="text-xl sm:text-2xl">Details of your purchase</h3>
-          <PlanDetailsBox plan={plan} />
+          <PlanDetailsBox plan={order.plan} />
         </>
       ) : isLoading ? (
         <Loader />
