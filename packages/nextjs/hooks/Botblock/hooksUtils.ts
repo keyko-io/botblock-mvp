8fc887366f0960245d014ca402bc6a87abca7101
Fabriziogianni7 <fabrizio@keyko.io>
2023-10-31 14:55:58 +0100

    botblock hooks

---
+++ packages/nextjs/hooks/Botblock/hooksUtils.ts
@@ -0,0 +1,20 @@
+import * as BotBlockMarketArtifact from "~~/public/artifacts/BotblockMarket.json";
+
+export const ContractsAndAbis = {
+  Botblock: {
+    address: "0xabe0D51F2f537c14CE782B26Fb3A59EB4A563316",
+    abi: BotBlockMarketArtifact.abi,
+  },
+};
+
+export enum ContractNames {
+  BOTBLOCK = "Botblock",
+}
+
+export enum BBFunctions {
+  CREATE_PLAN = "createPlan",
+}
+export type UseBBContractWrite = {
+  contractName: "Botblock";
+  functionName: string;
+};
---
+++ packages/nextjs/hooks/Botblock/useBBContractWrite.ts
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { ContractsAndAbis, UseBBContractWrite } from "./hooksUtils";
+import { useContractWrite } from "wagmi";
+import { notification } from "~~/utils/scaffold-eth";
+
+/**
+ * @param contractName: name of the contract (needed to load abi)
+ * @param functionName: name of the function
+ */
+
+export function useBBContractWrite({ contractName, functionName }: UseBBContractWrite) {
+  // address?: string
+  const [writeHook, setWriteHook] = useState<any>({});
+
+  const hook = useContractWrite({
+    address: ContractsAndAbis[contractName].address,
+    abi: ContractsAndAbis[contractName].abi as any[],
+    functionName,
+    onError(error) {
+      notification.error(`Transaction failed: ${error}`);
+    },
+    onSuccess(data) {
+      notification.success(`Transaction with hash ${data?.hash} completed successfully!`, {
+        icon: "🎉",
+      });
+    },
+  });
+
+  useEffect(() => {
+    setWriteHook(hook);
+  }, [hook]);
+
+  return { ...writeHook };
+
+  // const [isEthBalance, setIsEthBalance] = useState(true);
+  // const [balance, setBalance] = useState<number | null>(null);
+  // const price = useGlobalState(state => state.nativeCurrencyPrice);
+
+  // const {
+  //   data: fetchedBalanceData,
+  //   isError,
+  //   isLoading,
+  // } = useBalance({
+  //   address,
+  //   watch: true,
+  //   chainId: getTargetNetwork().id,
+  // });
+
+  // const onToggleBalance = useCallback(() => {
+  //   if (price > 0) {
+  //     setIsEthBalance(!isEthBalance);
+  //   }
+  // }, [isEthBalance, price]);
+
+  // useEffect(() => {
+  //   if (fetchedBalanceData?.formatted) {
+  //     setBalance(Number(fetchedBalanceData.formatted));
+  //   }
+  // }, [fetchedBalanceData]);
+
+  // return { balance, price, isError, isLoading, onToggleBalance, isEthBalance };
+}
