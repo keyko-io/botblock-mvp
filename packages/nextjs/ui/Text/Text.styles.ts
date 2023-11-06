9f2782b3f864528b46f17b726d5dc7f51aab5f31
juam <juanmanso@gmail.com>
2023-11-06 03:31:09 +0100

    feat(ui): create text component styles and types following the design

---
+++ packages/nextjs/ui/Text/Text.styles.ts
@@ -0,0 +1,84 @@
+import { CSSProperties } from "react";
+import { TextType } from "./Text.types";
+import { courierPrimeFont, exoFont } from "~~/public/assets/fonts";
+
+export const styles: Record<TextType, CSSProperties> = {
+  h1: {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "42px",
+    lineHeight: "1",
+    letterSpacing: "-1px",
+  },
+  h2: {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "24px",
+    lineHeight: "1",
+    letterSpacing: "0px",
+  },
+  h3: {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "18px",
+    lineHeight: "1",
+    letterSpacing: "0px",
+  },
+  subheading: {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "16px",
+    lineHeight: "107%",
+    letterSpacing: "0px",
+  },
+  "p-lg": {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "16px",
+    lineHeight: "125%",
+    letterSpacing: "0px",
+  },
+  "p-sm": {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "16px",
+    lineHeight: "150%",
+    letterSpacing: "-2%",
+  },
+  stats: {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "30px",
+    lineHeight: "100%",
+    letterSpacing: "0px",
+  },
+  "nav-link": {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "15px",
+    lineHeight: "100%",
+    letterSpacing: "0px",
+  },
+  label: {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "11px",
+    lineHeight: "100%",
+    letterSpacing: "0px",
+  },
+  "sm-print": {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "11px",
+    lineHeight: "100%",
+    letterSpacing: "0px",
+  },
+  "btn-lg": {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "15px",
+    lineHeight: "100%",
+    letterSpacing: "0px",
+  },
+  "btn-sm": {
+    fontFamily: exoFont.style.fontFamily,
+    fontSize: "12px",
+    lineHeight: "100%",
+    letterSpacing: "0px",
+  },
+  markdown: {
+    fontFamily: courierPrimeFont.style.fontFamily,
+    fontSize: "14px",
+    lineHeight: "140%",
+    letterSpacing: "0px",
+  },
+};
---
+++ packages/nextjs/ui/Text/Text.types.ts
@@ -0,0 +1,14 @@
+export type TextType =
+  | "h1"
+  | "h2"
+  | "h3"
+  | "subheading"
+  | "p-lg"
+  | "p-sm"
+  | "stats"
+  | "nav-link"
+  | "label"
+  | "sm-print"
+  | "btn-lg"
+  | "btn-sm"
+  | "markdown";
