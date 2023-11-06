00feca2fec142dc312b928c9e9d4b26cdca7f161
Fabriziogianni7 <fabrizio@keyko.io>
2023-11-06 10:44:46 +0000

    fixes on rainbowWeb3AuthConnector

--- packages/nextjs/services/web3/rainbowWeb3AuthConnector.tsx
+++ packages/nextjs/services/web3/rainbowWeb3AuthConnector.tsx
@@ -2,17 +2,27 @@ import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
 import { Web3AuthNoModal } from "@web3auth/no-modal";
 import { OPENLOGIN_NETWORK, OpenloginAdapter } from "@web3auth/openlogin-adapter";
 import { Web3AuthConnector } from "@web3auth/web3auth-wagmi-connector";
+import { Chain } from "viem";
 
 const name = "Login with Google";
 const iconUrl =
   "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-icon-logo-png-transparent-svg-vector-bie-supply-14.png";
- 
-//@ts-ignore
-export const rainbowWeb3AuthConnector = ({ chains }) => {
+
+type ChainConfig = {
+  chainNamespace: string | any;
+  chainId: string;
+  rpcTarget: string;
+  displayName: string;
+  blockExplorer: string;
+  ticker: string;
+  tickerName: string;
+}
+
+export const rainbowWeb3AuthConnector = ({ chains }: { chains: Chain[] }) => {
   // Create Web3Auth Instance
 
   const CLIENT_ID = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID;
-  const chainConfig = {
+  const chainConfig: ChainConfig = {
     chainNamespace: "eip155",
     chainId: "0x66eed",
     rpcTarget: "https://goerli-rollup.arbitrum.io/rpc",
@@ -24,7 +34,7 @@ export const rainbowWeb3AuthConnector = ({ chains }) => {
 
   const web3AuthInstance = new Web3AuthNoModal({
     clientId: CLIENT_ID as string,
-    chainConfig: chainConfig as any,
+    chainConfig: chainConfig as ChainConfig,
     web3AuthNetwork: OPENLOGIN_NETWORK.SAPPHIRE_MAINNET,
   });
 
@@ -36,7 +46,7 @@ export const rainbowWeb3AuthConnector = ({ chains }) => {
   web3AuthInstance.configureAdapter(openloginAdapterInstance);
 
   const connector = new Web3AuthConnector({
-    chains: chains,
+    chains,
     options: {
       web3AuthInstance,
       loginParams: {
