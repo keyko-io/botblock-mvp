8fdb3681704c9b038d1e8c345fd51d90eda68e5d
Fabriziogianni7 <fabrizio@keyko.io>
2023-11-08 13:41:42 +0100

    using publishAsset in confirm page (publish flow)

--- packages/nextjs/pages/unlock/partner/confirm.tsx
+++ packages/nextjs/pages/unlock/partner/confirm.tsx
@@ -3,9 +3,8 @@ import { Button } from "~~/components/Button";
 import { Loader } from "~~/components/Loader";
 import PlanDetailsBox from "~~/components/PlanDetailsBox";
 import { useWeb3AuthContext } from "~~/context/Web3AuthContext";
-import { useBBContractWrite } from "~~/hooks/Botblock";
-import { BBFunctions, ContractNames } from "~~/hooks/Botblock/hooksUtils";
-
+import { useNvmContext } from "~~/context/nvm/NvmContext";
+import { notification } from "~~/utils/scaffold-eth";
 
 const TITLE = "Confirm data and create a new plan";
 const DESCRIPTION = "Have a last check to the plan. when clicking confirm, the plan will be listed on Botblock market.";
@@ -13,17 +12,15 @@ const DESCRIPTION = "Have a last check to the plan. when clicking confirm, the p
 const Confirm = () => {
   const router = useRouter();
   const { plan } = useWeb3AuthContext();
-  const { isLoading, write } = useBBContractWrite({
-    contractName: ContractNames.BOTBLOCK,
-    functionName: BBFunctions.CREATE_PLAN,
-  });
+  const { publishAsset, payload, isNvmLoading } = useNvmContext();
 
-  const handleCreatePlan = async () => {
+  /**
+   * @dev handlePublishPlan uses nvm
+   */
+  const handlePublishPlan = async () => {
     try {
-      write({
-        args: [plan?.paymentTokenAddress, plan?.price, "1", plan?.uri],
-      });
-      router.push("/unlock/partner/next-steps");
+      const planId = await publishAsset(plan?.uri as string, plan?.price as string);
+      notification.success(`Plan successfully published with id: ${planId}`);
     } catch (error) {
       console.error;
     }
@@ -34,7 +31,8 @@ const Confirm = () => {
       <h1 className="text-4xl sm:text-6xl">{TITLE}</h1>
       <h3 className="text-xl sm:text-2xl">{DESCRIPTION}</h3>
       <div className="grid grid-cols-2 gap-4">{plan && <PlanDetailsBox title={"Recap"} plan={plan} />}</div>
-      {isLoading ? <Loader /> : <Button title={"Create Plan"} onClick={handleCreatePlan} />}
+      {!payload && <p>Please connect the wallet to publish the asset</p>}
+      {isNvmLoading ? <Loader /> : <Button title={"Publish using NVM"} onClick={handlePublishPlan} />}
     </div>
   );
 };
