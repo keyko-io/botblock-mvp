e9b7830e3e61e11e78453499b106a4ed7b7b6256
juam <juanmanso@gmail.com>
2023-11-08 21:24:40 +0100

    feat(landing): add header mock component

--- packages/nextjs/pages/landing.tsx
+++ packages/nextjs/pages/landing.tsx
@@ -1,13 +1,27 @@
+import toast from "react-hot-toast";
 import BlackGrungeImage from "~~/public/assets/images/black-grunge.png";
 import { palette } from "~~/styles/colors";
-import { Text } from "~~/ui";
+import { Button, Text } from "~~/ui";
+
+const Header = () => {
+  return (
+    <div className="flex flex-row justify-between items-center px-12 py-6 border-b-gray-500 border-b-2">
+      <Text type="p-lg">BotBlock | by Keyko powered by NVM</Text>
+      <Button onClick={() => toast.success("Log In button pressed")} color="ternary" icon="plus" size="sm">
+        Log In
+      </Button>
+    </div>
+  );
+};
 
 const Body = () => {
   return (
     <div
       className="flex flex-col"
       style={{ flex: 3, backgroundImage: `url("${BlackGrungeImage.src}")`, backgroundSize: "cover" }}
-    ></div>
+    >
+      <Header />
+    </div>
   );
 };
 
