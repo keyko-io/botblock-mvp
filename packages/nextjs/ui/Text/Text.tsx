25d0fe958762cf645fe55e6cd43eadf1fa8aeccf
juam <juanmanso@gmail.com>
2023-11-13 16:09:20 +0100

    feat(landing): implement basic footer

--- packages/nextjs/ui/Text/Text.tsx
+++ packages/nextjs/ui/Text/Text.tsx
@@ -4,15 +4,23 @@ import { Typography } from "./Text.types";
 
 interface TextProps {
   as?: ElementType;
+  onClick?: () => void;
   color?: "themed" | "light" | "dark";
   type?: Typography;
   style?: CSSProperties;
 }
 
-export const Text = ({ as = "p", children, color = "themed", type = "p-lg", style }: PropsWithChildren<TextProps>) => {
+export const Text = ({
+  as = "p",
+  children,
+  color = "themed",
+  type = "p-lg",
+  style,
+  ...rest
+}: PropsWithChildren<TextProps>) => {
   const Element = type === "label" ? "label" : as;
   return (
-    <Element style={{ ...baseTextStyles, ...textColorStyles[color], ...typographyStyles[type], ...style }}>
+    <Element {...rest} style={{ ...baseTextStyles, ...textColorStyles[color], ...typographyStyles[type], ...style }}>
       {children}
     </Element>
   );
