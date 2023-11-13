2c33bb46fd15511e6694bb18ad501cdd5661d6f6
juam <juanmanso@gmail.com>
2023-11-13 02:54:24 +0100

    feat(ui): dismiss label if not present

--- packages/nextjs/ui/Input/Input.tsx
+++ packages/nextjs/ui/Input/Input.tsx
@@ -6,13 +6,15 @@ interface InputProps extends HTMLProps<HTMLInputElement> {
   label?: string;
 }
 
-export const Input = ({ label = "Input Label", ...inputProps }: InputProps) => {
+export const Input = ({ label, value, ...inputProps }: InputProps) => {
   return (
     <div style={containerStyle}>
-      <Text type="label" style={labelStyle}>
-        {label}
-      </Text>
-      <input type="text" style={inputStyle} {...inputProps} />
+      {label ? (
+        <Text type="label" style={labelStyle}>
+          {label}
+        </Text>
+      ) : null}
+      <input type="text" style={inputStyle} value={value} {...inputProps} />
     </div>
   );
 };
