51da0719353ded4a9659a695ffac7537f07d2bce
juam <juanmanso@gmail.com>
2023-11-14 17:49:53 +0100

    fix(ui): footer layout

--- packages/nextjs/ui/Footer.tsx
+++ packages/nextjs/ui/Footer.tsx
@@ -1,7 +1,16 @@
+import { PropsWithChildren } from "react";
 import { useRouter } from "next/router";
 import { LargeLogo } from "./LargeLogo";
 import { Text } from "./Text/Text";
 import { palette } from "~~/styles/colors";
+
+const TextButton = ({ children, onClick }: PropsWithChildren<{ onClick: () => void }>) => {
+  return (
+    <Text as="button" onClick={onClick} type="btn-sm" color="light" style={{ width: "fit-content", padding: "4px" }}>
+      {children}
+    </Text>
+  );
+};
 
 export const Footer = () => {
   const router = useRouter();
@@ -11,26 +20,21 @@ export const Footer = () => {
   const browseToSubscribe = () => router.push("/subscribe");
 
   return (
-    <div className="flex flex-col px-12" style={{ flex: 1, backgroundColor: palette.slate[100] }}>
-      <div className="flex flex-row items-start my-12" style={{ flex: 2, minHeight: "110px" }}>
-        <div className="flex" style={{ flex: 1 }}>
-          <LargeLogo isLight />
-        </div>
-        <div className="flex flex-col justify-start items-start" style={{ flex: 1 }}>
-          <Text type="h3" color="light">
-            Sections
-          </Text>
-          <div className="flex flex-col mt-8 gap-4">
-            <Text as="button" onClick={browseToProtect} type="btn-sm" color="light">
-              Protect
-            </Text>
-            <Text as="button" onClick={browseToPartner} type="btn-sm" color="light">
-              Partner
-            </Text>
-            <Text as="button" onClick={browseToSubscribe} type="btn-sm" color="light">
-              Subscribe
-            </Text>
-          </div>
+    <div
+      className="flex flex-row items-start my-12 p-12"
+      style={{ flex: 1, height: "110px", backgroundColor: palette.slate[100] }}
+    >
+      <div className="flex-1">
+        <LargeLogo isLight />
+      </div>
+      <div className="flex flex-col flex-1 justify-start items-start">
+        <Text type="h3" color="light">
+          Sections
+        </Text>
+        <div className="flex flex-col mt-4 gap-2">
+          <TextButton onClick={browseToProtect}>Protect</TextButton>
+          <TextButton onClick={browseToPartner}>Partner</TextButton>
+          <TextButton onClick={browseToSubscribe}>Subscribe</TextButton>
         </div>
       </div>
     </div>
