bbb91b05d8af7752ad1dd8d0820e9535bc4e28cb
juam <juanmanso@gmail.com>
2023-11-15 12:37:31 +0100

    feat(ui): add state when modal is open on login button

--- packages/nextjs/ui/Header/LoginButton.tsx
+++ packages/nextjs/ui/Header/LoginButton.tsx
@@ -5,14 +5,20 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 export const LoginButton = () => {
   return (
     <ConnectButton.Custom>
-      {({ account, chain, openConnectModal, mounted }) => {
+      {({ account, chain, openConnectModal, mounted, connectModalOpen }) => {
         const connected = mounted && account && chain;
         return connected ? (
           // TODO: tweak this UI
           <Text>Connected</Text>
         ) : (
-          <Button onClick={openConnectModal} color="ternary" icon="plus" size="sm">
-            Log In
+          <Button
+            onClick={openConnectModal}
+            disabled={connectModalOpen}
+            color="ternary"
+            icon={connectModalOpen ? undefined : "plus"}
+            size="lg"
+          >
+            {connectModalOpen ? "Logging in..." : "Log In"}
           </Button>
         );
       }}
