7c4aa3d9ed92f6972a0e2ea0e1592f6799b808d3
juam <juanmanso@gmail.com>
2023-11-15 15:20:53 +0100

    feat(ui): cleanup and use new design on header

--- packages/nextjs/ui/Header/index.tsx
+++ packages/nextjs/ui/Header/index.tsx
@@ -1,6 +1,8 @@
 import React, { PropsWithChildren } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { LargeLogo } from "../LargeLogo";
+import { LoginButton } from "./LoginButton";
 
 const NavLink = ({ children, href }: PropsWithChildren<{ href: string }>) => {
   const router = useRouter();
@@ -23,14 +25,6 @@ const NavLink = ({ children, href }: PropsWithChildren<{ href: string }>) => {
  * Site header
  */
 export const Header = () => {
-  const { isConnected } = useWeb3AuthContext();
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const burgerMenuRef = useRef<HTMLDivElement>(null);
-  useOutsideClick(
-    burgerMenuRef,
-    useCallback(() => setIsDrawerOpen(false), []),
-  );
-
   const navLinks = (
     <ul className="flex flex-row ml-32">
       <li>
@@ -46,44 +40,13 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
-      <div className="navbar-start w-auto lg:w-1/2">
-        <div className="lg:hidden dropdown" ref={burgerMenuRef}>
-          <label
-            tabIndex={0}
-            className={`ml-1 btn btn-ghost ${isDrawerOpen ? "hover:bg-secondary" : "hover:bg-transparent"}`}
-            onClick={() => {
-              setIsDrawerOpen(prevIsOpenState => !prevIsOpenState);
-            }}
-          >
-            <Bars3Icon className="h-1/2" />
-          </label>
-          {isDrawerOpen && (
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-              onClick={() => {
-                setIsDrawerOpen(false);
-              }}
-            >
-              {navLinks}
-            </ul>
-          )}
-        </div>
-        <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
-          <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold leading-tight">Botblock</span>
-            <span className="text-xs">Get paid from ai</span>
-          </div>
-        </Link>
-        <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
+    <div className="flex flex-row justify-between items-center px-12 py-6 border-b-gray-500 border-b-2">
+      <div className="flex flex-row">
+        <LargeLogo />
+        <div className="w-8" />
+        {navLinks}
       </div>
-      <div className="navbar-end flex-grow mr-4">
-        <RainbowKitCustomConnectButton />
-      </div>
+      <LoginButton />
     </div>
   );
 };
