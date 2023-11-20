59917e030c8eda065c6b3acd5276df49f416a971
juam <juanmanso@gmail.com>
2023-11-20 16:48:46 +0100

    feat(profile): replace all html text markup with Text comp

--- packages/nextjs/pages/profile/index.tsx
+++ packages/nextjs/pages/profile/index.tsx
@@ -5,6 +5,7 @@ import { Order, Plan, TokenAddress, tokenAddressMap } from "~~/context/Types";
 import { useWeb3AuthContext } from "~~/context/Web3AuthContext";
 import { useBBContractReads } from "~~/hooks/Botblock";
 import { ContractNames } from "~~/hooks/Botblock/hooksUtils";
+import { Text } from "~~/ui";
 
 const Profile = () => {
   const [userOrders, setUserOrders] = useState<Order[]>();
@@ -27,42 +28,62 @@ const Profile = () => {
 
   if (!isConnected) {
     return (
-      <div className="p-32 flex-grow" data-theme="exampleUi">
-        <h1 className="text-2xl sm:text-3xl my-8">Please log in to access this area</h1>
+      <div className="p-32 flex-grow">
+        <Text type="h2" style={{ margin: "24px 0" }}>
+          Please log in to access this area
+        </Text>
         <RainbowKitCustomConnectButton />
       </div>
     );
   }
 
   return (
-    <div className="p-32 flex-grow" data-theme="exampleUi">
-      <h1 className="text-2xl sm:text-3xl">Welcome to your profile, {username}</h1>
+    <div className="p-32 flex-grow">
+      <Text type="h1">Welcome to your profile, {username}</Text>
       <>
         <div className="grid grid-cols-2 gap-4 mt-4 mb-16">
           <div className="container w-fit">
             <div className="bg-gray-200 shadow-md overflow-x-auto rounded p-4">
-              <h2 className="text-2xl font-bold mb-4">Your orders</h2>
-              <table className="w-full border-collapse bg-white">
+              <Text type="h2" style={{ marginBottom: "8px" }}>
+                Your orders
+              </Text>
+              <table className="w-full border-collapse bg-black">
                 <thead>
                   <tr>
-                    <th className="border p-2">Website</th>
-                    <th className="border p-2">Duration</th>
-                    <th className="border p-2">Status</th>
+                    <Text as="th" type="subheading" style={{ borderWidth: "1px", padding: "8px" }}>
+                      Website
+                    </Text>
+                    <Text as="th" type="subheading" style={{ borderWidth: "1px", padding: "8px" }}>
+                      Duration
+                    </Text>
+                    <Text as="th" type="subheading" style={{ borderWidth: "1px", padding: "8px" }}>
+                      Status
+                    </Text>
                   </tr>
                 </thead>
                 <tbody>
                   {userOrders?.length ? (
                     userOrders.map((order, index) => (
                       <tr key={index}>
-                        <td className="border p-2 text-center">{order.plan.uri}</td>
-                        <td className="border p-2 text-center">
+                        <Text as="th" style={{ textAlign: "center", padding: "8px", borderWidth: "1px" }}>
+                          {order.plan.uri}
+                        </Text>
+                        <Text as="th" style={{ textAlign: "center", padding: "8px", borderWidth: "1px" }}>
                           {order.plan.expirationBlock} Month{Number(order.plan.expirationBlock) > 1 ? "s" : ""}
-                        </td>
-                        <td className="border p-2 text-end">{order.status}</td>
+                        </Text>
+                        <Text as="th" style={{ textAlign: "center", padding: "8px", borderWidth: "1px" }}>
+                          {order.status}
+                        </Text>
                       </tr>
                     ))
                   ) : (
-                    <p> Looks like there are no subscriptions associated with this address!</p>
+                    <tr>
+                      <td colSpan={3}>
+                        <Text style={{ textAlign: "center" }}>
+                          Looks like there are no subscriptions associated with this address!
+                        </Text>
+                      </td>
+                    </tr>
                   )}
                 </tbody>
               </table>
@@ -72,30 +93,46 @@ const Profile = () => {
         <div className="grid grid-cols-2 gap-4">
           <div className="container w-fit">
             <div className="bg-gray-200 shadow-md overflow-x-auto rounded p-4">
-              <h2 className="text-2xl font-bold mb-4">Your plans</h2>
+              <Text type="h2" style={{ marginBottom: "8px" }}>
+                Your plans
+              </Text>
               <table className="w-full border-collapse bg-white">
                 <thead>
                   <tr>
-                    <th className="border p-2">Website</th>
-                    <th className="border p-2">Duration</th>
-                    <th className="border p-2">Price</th>
+                    <Text as="th" type="subheading" style={{ borderWidth: "1px", padding: "8px" }}>
+                      Website
+                    </Text>
+                    <Text as="th" type="subheading" style={{ borderWidth: "1px", padding: "8px" }}>
+                      Duration
+                    </Text>
+                    <Text as="th" type="subheading" style={{ borderWidth: "1px", padding: "8px" }}>
+                      Price
+                    </Text>
                   </tr>
                 </thead>
                 <tbody>
                   {userPlans?.length ? (
                     userPlans.map((plan, index) => (
                       <tr key={index}>
-                        <td className="border p-2 text-center">{plan.uri}</td>
-                        <td className="border p-2 text-center">
+                        <Text as="th" style={{ textAlign: "center", padding: "8px", borderWidth: "1px" }}>
+                          {plan.uri}
+                        </Text>
+                        <Text as="th" style={{ textAlign: "center", padding: "8px", borderWidth: "1px" }}>
                           {plan.expirationBlock} Month{plan.expirationBlock !== "1" && "s"}
-                        </td>
-                        <td className="border p-2 text-end">
+                        </Text>
+                        <Text as="th" style={{ textAlign: "end", padding: "8px", borderWidth: "1px" }}>
                           {plan.price} {tokenAddressMap[plan.paymentTokenAddress as TokenAddress]}
-                        </td>
+                        </Text>
                       </tr>
                     ))
                   ) : (
-                    <p> Looks like there are no plans created with this address!</p>
+                    <tr>
+                      <td colSpan={3}>
+                        <Text style={{ textAlign: "center" }}>
+                          Looks like there are no plans created with this address!
+                        </Text>
+                      </td>
+                    </tr>
                   )}
                 </tbody>
               </table>
