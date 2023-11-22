9953835cbfd12f849d1eea95550091722f9f0a3c
hcsoylu <hcsoylu.dev@gmail.com>
2023-11-22 18:16:18 +0300

    feat(protect): adjust style and functionality fix

--- packages/nextjs/ui/Button/Button.tsx
+++ packages/nextjs/ui/Button/Button.tsx
@@ -23,8 +23,9 @@ interface ButtonProps extends PropsWithChildren {
   fullWidth?: boolean;
   icon?: ButtonIcons;
   isLoading?: boolean;
-  onClick: () => void;
+  onClick?: () => void;
   size?: "sm" | "lg";
+  type?: "submit";
 }
 
 export const Button = ({
@@ -36,10 +37,12 @@ export const Button = ({
   isLoading,
   onClick,
   size = "sm",
+  type,
 }: ButtonProps) => {
   const MappedIcon = iconMap[icon as ButtonIcons];
   return (
     <button
+      type={type}
       onClick={onClick}
       disabled={disabled}
       style={{
