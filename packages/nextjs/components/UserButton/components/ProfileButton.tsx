2af1fc70f01b64d65a7893ae9e408d726b4ce04d
juam <juanmanso@gmail.com>
2023-11-22 04:31:44 +0100

    feat(components): tweak profile button to give expected look

--- packages/nextjs/components/UserButton/components/ProfileButton.tsx
+++ packages/nextjs/components/UserButton/components/ProfileButton.tsx
@@ -1,11 +1,38 @@
+import { SVGProps } from "react";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { coreColors, palette } from "~~/styles/colors";
+import { Row } from "~~/ui";
+
+const ChevronIcon = dynamic<SVGProps<SVGSVGElement>>(() => import("~~/public/assets/icons/chevron.svg"));
+const ScribbleIcon = dynamic<SVGProps<SVGSVGElement>>(() => import("~~/public/assets/icons/scribble.svg"));
+
 interface ProfileButtonProps {
   name?: string;
 }
 
-export const ProfileButton = ({ name }: ProfileButtonProps) => {
+export const ProfileButton = ({}: ProfileButtonProps) => {
+  const router = useRouter();
+
+  const browseToProfile = () => router.push("/profile");
+
   return (
-    <button className="btn btn-primary btn-sm" type="button">
-      {name && `[${name}] `}→ Log out
+    <button onClick={browseToProfile} style={{ position: "relative", width: "57px" }}>
+      <ScribbleIcon
+        color={palette.purple[100]}
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%) rotate(-160deg)",
+          width: "83px",
+          height: "auto",
+        }}
+      />
+      <Row style={{ position: "absolute", top: "50%", left: "45%", transform: "translate(-55%, -50%)", gap: "4px" }}>
+        <p style={{ fontSize: "27px" }}>🧑‍⚕️</p>
+        <ChevronIcon color={coreColors.white} style={{ transform: "rotate(-90deg)", marginTop: "8px" }} />
+      </Row>
     </button>
   );
 };
