59c4cd4f3f209f4d553fcf4dfbecb3eff2fd5c0e
juam <juanmanso@gmail.com>
2023-11-29 04:05:06 +0100

    feat(ui): add column, row and chevron comps to select without css

--- packages/nextjs/ui/Select.tsx
+++ packages/nextjs/ui/Select.tsx
@@ -1,32 +1,42 @@
-import { InputHTMLAttributes, useState } from "react";
+import { InputHTMLAttributes, SVGProps, useState } from "react";
+import dynamic from "next/dynamic";
+import { Column } from "./Column";
 import { Dropdown } from "./Dropdown/Dropdown";
+import { Row } from "./Row";
 import { Text } from "./Text/Text";
-import { palette } from "~~/styles/colors";
+import { coreColors } from "~~/styles/colors";
+
+const ChevronIcon = dynamic<SVGProps<SVGSVGElement>>(() => import("~~/public/assets/icons/chevron.svg"));
 
 interface SelectProps extends Omit<InputHTMLAttributes<any>, "onChange"> {
   label?: string;
+  selected: string;
 }
 
-export const Select = ({ id, label }: SelectProps) => {
+export const Select = ({ id, label, selected }: SelectProps) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const openDropdown = () => setIsDropdownOpen(true);
   const closeDropdown = () => setIsDropdownOpen(false);
 
   return (
-    <>
-      <button id={id} onClick={openDropdown}>
-        {label && (
-          <Text htmlFor={id} type="label">
-            {label}
-          </Text>
-        )}
-      </button>
-      <Dropdown
-        isOpen={isDropdownOpen}
-        close={closeDropdown}
-        style={{ backgroundColor: palette.slate[100] }}
-      ></Dropdown>
-    </>
+    <Column>
+      <Row as="button" id={id} disabled={isDropdownOpen} onClick={openDropdown}>
+        <Column>
+          {label && (
+            <Text htmlFor={id} type="label">
+              {label}
+            </Text>
+          )}
+          <Text>{selected}</Text>
+        </Column>
+        <Column>
+          <ChevronIcon color={coreColors.lightGray} style={{ transform: "rotate(-90deg)" }} />
+        </Column>
+      </Row>
+      <Dropdown isOpen={isDropdownOpen} close={closeDropdown}>
+        <Column></Column>
+      </Dropdown>
+    </Column>
   );
 };
