dc78d3d9e750e5dbf21381a3b0cdba168f257cb6
juam <juanmanso@gmail.com>
2023-11-29 04:13:26 +0100

    feat(ui): tweak styles to match design

--- packages/nextjs/ui/Select.tsx
+++ packages/nextjs/ui/Select.tsx
@@ -4,7 +4,7 @@ import { Column } from "./Column";
 import { Dropdown } from "./Dropdown/Dropdown";
 import { Row } from "./Row";
 import { Text } from "./Text/Text";
-import { coreColors } from "~~/styles/colors";
+import { coreColors, palette } from "~~/styles/colors";
 
 const ChevronIcon = dynamic<SVGProps<SVGSVGElement>>(() => import("~~/public/assets/icons/chevron.svg"));
 
@@ -13,16 +13,35 @@ interface SelectProps extends Omit<InputHTMLAttributes<any>, "onChange"> {
   selected: string;
 }
 
-export const Select = ({ id, label, selected }: SelectProps) => {
+export const Select = ({ disabled, id, label, selected }: SelectProps) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const openDropdown = () => setIsDropdownOpen(true);
   const closeDropdown = () => setIsDropdownOpen(false);
 
   return (
-    <Column>
-      <Row as="button" id={id} disabled={isDropdownOpen} onClick={openDropdown}>
-        <Column>
+    <Column
+      style={{
+        display: "flex",
+        position: "relative",
+        alignItems: "center",
+        width: undefined,
+      }}
+    >
+      <Row
+        as="button"
+        id={id}
+        disabled={isDropdownOpen || disabled}
+        onClick={openDropdown}
+        style={{
+          borderWidth: "1px",
+          borderRadius: "8px",
+          padding: "12px 20px",
+          gap: "16px",
+          backgroundColor: palette.slate[100],
+        }}
+      >
+        <Column style={{ alignItems: "flex-start", flex: 1, width: "100%", ...(label && { gap: "8px" }) }}>
           {label && (
             <Text htmlFor={id} type="label">
               {label}
@@ -30,11 +49,17 @@ export const Select = ({ id, label, selected }: SelectProps) => {
           )}
           <Text>{selected}</Text>
         </Column>
-        <Column>
+        <Column style={{ alignItems: "flex-end", width: undefined }}>
           <ChevronIcon color={coreColors.lightGray} style={{ transform: "rotate(-90deg)" }} />
         </Column>
       </Row>
-      <Dropdown isOpen={isDropdownOpen} close={closeDropdown}>
+      <Dropdown
+        isOpen={isDropdownOpen}
+        close={closeDropdown}
+        style={{
+          backgroundColor: palette.slate[100],
+        }}
+      >
         <Column></Column>
       </Dropdown>
     </Column>
